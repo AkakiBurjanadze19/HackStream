6abdabcf0e0e18278a18ba58ec5902d84cdacfa3
AkakiBurjanadze <akakiburjanadze17@gmail.com>
2025-12-13 13:50:00 +0400

    add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TaskCard from "./TaskCard";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const task = {
+  title: "Implement drag & drop",
+  description: "Enable moving tasks across columns with keyboard + mouse support.",
+  deadline: "2025-12-20T18:00:00Z",
+  hours_left_until_deadline: 42,
+  creation_date: "2025-12-12",
+  importance: 9,
+  effort: 4,
+};
 
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ 
+      width: "100%",
+      display: "flex",
+      justifyContent: "flex-end"
+    }}>
+      <div style={{ 
+        padding: 20, 
+        width: 900,
+        maxWidth: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-end",
+        gap: 20
+      }}>
+        <TaskCard task={task} status="ready" />
+        <TaskCard task={task} status="blocked" />
+        <TaskCard task={task} status="in_progress" />
+        <TaskCard task={task} status="overdue" />
+        <TaskCard task={task} status="blocked" />
+        <TaskCard task={task} status="blocked" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
